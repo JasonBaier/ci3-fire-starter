8734326abcbc41fe4870e6108f46bea8924f90ab
arif-rh <arifrahmanhakim.net@gmail.com>
2015-03-03 08:35:10 +0700

    add indonesian admin_lang.php

---
+++ application/language/indonesian/admin_lang.php
@@ -0,0 +1,37 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Admin Indonesian Language File 
+ */
+
+// Titles
+$lang['admin title admin']                = "Halaman Admin";
+
+// Buttons
+$lang['admin button csv_export']          = "Ekspor CSV";
+$lang['admin button dashboard']           = "Dashboard";
+$lang['admin button delete']              = "Hapus";
+$lang['admin button edit']                = "Ubah";
+$lang['admin button messages']            = "Pesan";
+$lang['admin button settings']            = "Pengaturan";
+$lang['admin button users']               = "User";
+$lang['admin button users_add']           = "Tambah User Baru";
+$lang['admin button users_list']          = "Daftar User";
+
+// Tooltips
+$lang['admin tooltip csv_export']         = "Ekspor file CSV dari semua hasil dengan penerapan penyaringan.";
+$lang['admin tooltip filter']             = "Perbarui hasil berdasarkan penyaringan anda.";
+$lang['admin tooltip filter_reset']       = "Bersihkan semua penyaringan dan pengurutan anda.";
+
+// Form Inputs
+$lang['admin input active']               = "Aktif";
+$lang['admin input inactive']             = "Non Aktif";
+$lang['admin input items_per_page']       = "item/halaman";
+$lang['admin input select']               = "pilih...";
+$lang['admin input username']             = "Username";
+
+// Table Columns
+$lang['admin col actions']                = "Aksi";
+$lang['admin col status']                 = "Status";
+
+// Form Labels
+$lang['admin label rows']                 = "% baris";
