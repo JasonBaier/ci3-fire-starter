d91be06e9d9850099c2ceb6863e9468f23a386f2
arif-rh <arifrahmanhakim.net@gmail.com>
2015-03-03 08:39:29 +0700

    add indonesian contact_lang.php

---
+++ application/language/indonesian/contact_lang.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Contact Indonesian Language File
+ */
+
+// Titles
+$lang['contact title']                  = "Hubungi Kami";
+$lang['contact title messages_list']    = "Pesan";
+
+// Buttons
+$lang['contact button read']            = "Baca Pesan";
+
+// Table Columns
+$lang['contact col message_id']         = "ID";
+$lang['contact col name']               = "Nama";
+$lang['contact col email']              = "Email";
+$lang['contact col title']              = "Judul";
+$lang['contact col created']            = "Diterima";
+
+// Form Inputs
+$lang['contact input name']             = "Nama";
+$lang['contact input email']            = "Email";
+$lang['contact input title']            = "Judul";
+$lang['contact input message']          = "Pesan";
+$lang['contact input captcha']          = "Teks CAPTCHA";
+$lang['contact input created']          = "Diterima";
+
+// Messages
+$lang['contact msg send_success']       = "Terima kasih sudah menghubungi kami, %s! Pesan anda telah terkirim.";
+$lang['contact msg updated']            = "Pesan diperbarui!";
+
+// Errors
+$lang['contact error captcha']          = "Teks CAPTCHA tidak cocok.";
+$lang['contact error send_failed']      = "Maaf, %s. Terdapat masalah dalam pengiriman pesan anda. Silakan coba lagi.";
+$lang['contact error update_failed']    = "Tidak dapat memperbarui status.";
