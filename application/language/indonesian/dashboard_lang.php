b2f042ed181a65827a3506b7eaacacacaf913c1c
arif-rh <arifrahmanhakim.net@gmail.com>
2015-03-04 14:05:36 +0700

    add indonesian dashboard_lang.php

---
+++ application/language/indonesian/dashboard_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Admin Dashboard Language File
+ */
+
+// Jsi18n Demo
+$lang['admin dashboard jsi18n-sample'] = "Ini merupakan demo penggunaan librari Jsi18n. Librari ini mengambil teks dari file bahasa dan memasukkannya ke dalam Javascripts anda. Lihatlah file jsi18n.php library dan dashboard_i18n.js untuk penggunaannya.";
