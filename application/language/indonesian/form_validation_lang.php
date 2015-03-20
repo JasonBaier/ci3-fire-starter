b193c0daa566862b731947816d9ffa0feca8406e
arif-rh <arifrahmanhakim.net@gmail.com>
2015-03-20 08:53:54 +0700

    add more language files

---
+++ application/language/indonesian/form_validation_lang.php
@@ -0,0 +1,34 @@
+<?php	defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Form Validation Language
+ */
+
+$lang['form_validation_required']		= 'Field {field} harus diisi.';
+$lang['form_validation_isset']			= 'Field {field} harus punya nilai.';
+$lang['form_validation_valid_email']		= 'Field {field} harus berisi alamat email yang sah.';
+$lang['form_validation_valid_emails']		= 'Field {field} harus berisi semua alamat email yang sah.';
+$lang['form_validation_valid_url']		= 'Field {field} harus berisi URL yang sah.';
+$lang['form_validation_valid_ip']		= 'Field {field} harus berisi alamat IP yang sah.';
+$lang['form_validation_min_length']		= 'Field {field} harus diisi paling sedikit {param} karakter panjangnya.';
+$lang['form_validation_max_length']		= 'Field {field} tidak boleh melebihi {param} karakter panjangnya.';
+$lang['form_validation_exact_length']		= 'Field {field} harus persis {param} karakter panjangnya.';
+$lang['form_validation_alpha']			= 'Field {field} hanya boleh diisi karakter alfabet.';
+$lang['form_validation_alpha_numeric']		= 'Field {field} hanya boleh diisi karakter alfa-numerik.';
+$lang['form_validation_alpha_numeric_spaces']	= 'Field {field} hanya boleh diisi karakter alfa-numerik dan spasi.';
+$lang['form_validation_alpha_dash']		= 'Field {field} hanya boleh diisi karakter alfa-numerik, garis bawah, dan tanda hubung.';
+$lang['form_validation_numeric']		= 'Field {field} harus diisi dengan angka saja.';
+$lang['form_validation_is_numeric']		= 'Field {field} harus berisi karakter angka saja.';
+$lang['form_validation_integer']		= 'Field {field} harus berisi bilangan integer.';
+$lang['form_validation_regex_match']		= 'Field {field} tidak sesuai dengan format yang benar.';
+$lang['form_validation_matches']		= 'Field {field} tidak cocok dengan field {param}.';
+$lang['form_validation_differs']		= 'Field {field} harus berbeda dari field {param}.';
+$lang['form_validation_is_unique'] 		= 'Field {field} harus berisi nilai yang unik.';
+$lang['form_validation_is_natural']		= 'Field {field} harus berisi digit saja.';
+$lang['form_validation_is_natural_no_zero']	= 'Field {field} harus berisi digit saja dan harus lebih besar dari nol.';
+$lang['form_validation_decimal']		= 'Field {field} harus berisi angka desimal.';
+$lang['form_validation_less_than']		= 'Field {field} harus berisi angka lebih kecil dari {param}.';
+$lang['form_validation_less_than_equal_to']	= 'Field {field} harus berisi angka lebih kecil atau sama dengan {param}.';
+$lang['form_validation_greater_than']		= 'Field {field} harus berisi angka lebih besar dari {param}.';
+$lang['form_validation_greater_than_equal_to']	= 'Field {field} harus berisi angka lebih besar atau sama dengan {param}.';
+$lang['form_validation_error_message_not_set']	= 'Tidak dapat mengakses pesan kesalahan terkait nama field {field} Anda.';
+$lang['form_validation_in_list']		= 'Field {field} harus salah satu dari: {param}.';
---
+++ application/language/indonesian/upload_lang.php
@@ -0,0 +1,21 @@
+<?php	defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Upload Language File
+ */
+
+$lang['upload_userfile_not_set'] = 'Tidak dapat menemukan variable post dengan nama userfile.';
+$lang['upload_file_exceeds_limit'] = 'File yang diunggah melebihi batas maksimal ukuran yang diperbolehkan dalam file konfigurasi PHP Anda.';
+$lang['upload_file_exceeds_form_limit'] = 'File yang diunggah melebihi batas maksimal ukuran yang diperbolehkan oleh ketentuan form.';
+$lang['upload_file_partial'] = 'File hanya terunggah sebagian saja.';
+$lang['upload_no_temp_directory'] = 'Folder sementara tidak ada.';
+$lang['upload_unable_to_write_file'] = 'File tidak dapat disimpan ke disk.';
+$lang['upload_stopped_by_extension'] = 'Unggah file dihentikan karena ekstensi.';
+$lang['upload_no_file_selected'] = 'Anda tidak memilih sebuah file untuk diunggah.';
+$lang['upload_invalid_filetype'] = 'Jenis file yang Anda coba unggah tidak diperbolehkan.';
+$lang['upload_invalid_filesize'] = 'File yang Anda coba unggah lebih besar dari ukuran yang diijinkan.';
+$lang['upload_invalid_dimensions'] = 'Gambar yang Anda coba unggah tidak sesuai dengan dimensi yang diperbolehkan.';
+$lang['upload_destination_error'] = 'Sebuah masalah terjadi saat mencoba memindahkan file yang diunggah ke tujuan akhir.';
+$lang['upload_no_filepath'] = 'Path unggahan tampak tidak benar.';
+$lang['upload_no_file_types'] = 'Anda tidak menentukan jenis file yang diperbolehkan apa pun .';
+$lang['upload_bad_filename'] = 'Nama file yang Anda kirim sudah ada di server.';
+$lang['upload_not_writable'] = 'Folder tujuan unggahan tampak tidak dapat ditulis.';
