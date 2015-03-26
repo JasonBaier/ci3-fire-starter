73d0cdd444c0d72ad940dc00ae5fcbf826a8d0cf
arif-rh <arifrahmanhakim.net@gmail.com>
2015-03-26 08:43:36 +0700

    more indonesian lang
    add db_lang.php, email_lang.php and fix some words.

--- application/language/indonesian/users_lang.php
+++ application/language/indonesian/users_lang.php
@@ -1,6 +1,6 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php	defined('BASEPATH') OR exit('Akses skrip secara langsung tidak diijinkan');
 /**
- * Users Language File
+ * File Bahasa Indonesia - Users
  */
 
 // Titles
