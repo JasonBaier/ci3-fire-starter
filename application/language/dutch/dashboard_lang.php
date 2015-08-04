ee7d640f1d98b487b408ee7e07f04a372fe4f57a
DeeJaVu <ddejjavvu@gmail.com>
2015-08-04 06:38:48 +0200

    Dutch lang

---
+++ application/language/dutch/dashboard_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Admin Dashboard Language File
+ */
+
+// Jsi18n Demo
+$lang['admin dashboard jsi18n-sample'] = "Dit is een demonstratie van de Jsi18n bibliotheek. Het neemt tekst uit een taalbestand en voegt het in uw Javascripts. Zie de jsi18n.php bibliotheek en dashboard_i18n.js voor gebruik.";
