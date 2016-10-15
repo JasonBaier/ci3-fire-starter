b16f7803401d023f24c26179b2809f70f5660fac
satrio arif wicaksono <ssatrioaw@gmail.com>
2016-10-15 17:54:31 +0700

    composer autoload was not work

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database', 'session', 'form_validation', 'pagination', 'email', 'jsi18n');
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'pagination', 'email', 'jsi18n','my_composer');
 
 
 /*
---
+++ application/libraries/My_composer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of MY_Composer
+ *
+ * @author Rana
+ */
+class MY_Composer 
+{
+    function __construct() 
+    {
+        include("../vendor/autoload.php");
+    }
+}
