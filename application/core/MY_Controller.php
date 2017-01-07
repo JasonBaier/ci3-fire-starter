483376efeb6268aa24ab2de8727d6824c53e632c
Arif RH <arifrahmanhakim.net@gmail.com>
2017-01-07 14:36:45 +0700

    add base_url to core.css
    without base_url, it will be 404 when application run in sub folder

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -84,7 +84,7 @@ class MY_Controller extends CI_Controller {
                     "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
                     "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css",
                     "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-                    "/themes/core/css/core.css"
+                    base_url("/themes/core/css/core.css")
                 ))
             ->add_external_js(
                 array(
