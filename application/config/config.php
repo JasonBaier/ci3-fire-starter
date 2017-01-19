6a0663c5e232e265393e46582435a1a46f2fd3bb
iamchiriru <chithewebdeveloper@gmail.com>
2017-01-20 01:33:11 +0800

    Dev Init Commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci3-fire-starter.dev/';
+$config['base_url'] = 'http://iamchiriru-me.local/';
 
 /*
 |--------------------------------------------------------------------------
